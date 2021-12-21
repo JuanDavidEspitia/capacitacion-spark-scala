260a49f79aecbd7c0985dc3a4856e56c004e4622
Juan David Espitia <juandespitia14@gmail.com>
2021-12-21 15:41:48 -0500

    Init params and spark session

--- src/main/scala/net/capacitacion/Demo.scala
+++ src/main/scala/net/capacitacion/Demo.scala
@@ -1,10 +1,27 @@
 package net.capacitacion
 
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
 object Demo {
+
+  var nameApp = "CapacitacionSpark"
+  var pathInput = "input/MOCK_DATA_1.csv"
+  var delimitador = "|"
+
+  val spark: SparkSession = SparkSession.builder
+    .appName(nameApp)
+    .master("local[*]")
+    .getOrCreate()
+
+  var dfInputData: DataFrame = spark.emptyDataFrame
+
 
   def main(args: Array[String]): Unit = {
 
-    println("Hola Mundo")
+
+
+
+
 
 
   }
