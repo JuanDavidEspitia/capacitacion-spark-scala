53a0de993e849ce84170f658818debd2338a4a81
Juan David Espitia <juandespitia14@gmail.com>
2021-12-21 15:58:28 -0500

    update objet demo (Load file in df)

--- src/main/scala/net/capacitacion/Demo.scala
+++ src/main/scala/net/capacitacion/Demo.scala
@@ -1,5 +1,6 @@
 package net.capacitacion
 
+import org.apache.spark.SparkContext
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
 object Demo {
@@ -7,21 +8,32 @@ object Demo {
   var nameApp = "CapacitacionSpark"
   var pathInput = "input/MOCK_DATA_1.csv"
   var delimitador = "|"
+  var sc: SparkContext = null
 
   val spark: SparkSession = SparkSession.builder
     .appName(nameApp)
     .master("local[*]")
     .getOrCreate()
 
+  println("Hola mundo")
+
   var dfInputData: DataFrame = spark.emptyDataFrame
 
 
   def main(args: Array[String]): Unit = {
 
+    sc = spark.sparkContext
 
+    dfInputData = spark.read
+      .option("delimiter", delimitador)
+      .option("header", "true")
+      .option("charset", "UTF-8")
+      .option("inferSchema", "true")
+      .option("nullValue", "null")
+      .csv(pathInput)
 
-
-
+    dfInputData.show(false)
+    dfInputData.printSchema()
 
 
   }
